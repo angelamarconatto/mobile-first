657d4de6b3632753c13793c37499383ff3ee0bb4
Angela <159147808+angelamarconatto@users.noreply.github.com>
2024-08-29 20:53:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+document.getElementById('burger-menu').addEventListener('click', function() {
+    var navLinks = document.getElementById('nav-links');
+    navLinks.classList.toggle('show');
+});
